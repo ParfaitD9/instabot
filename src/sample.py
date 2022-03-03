92c4cc5c9764f644ea1ad5faac831114dfd981d2
Parfait_D <pdetchenou@gmail.com>
2022-03-03 11:04:27 +0100

    remove sample

--- src/sample.py
+++
@@ -1,6 +0,0 @@
-from instadm import InstaDM
-
-if __name__ == '__main__':
-    acc = InstaDM(userame= 'pdetchenou@gmail.com', password= '32Dexembre', headless= False)
-
-    acc.sendMessage(user= 'instagram', message= 'Hey !')
